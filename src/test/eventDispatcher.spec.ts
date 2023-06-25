09b33f223231a6f9af9d27a3861a73d5a18a1257
Wander Aquino <wander.aquino@gmail.com>
2023-06-25 13:19:15 -0300

    Track eventDispatcher test file for send email event

---
+++ src/test/eventDispatcher.spec.ts
@@ -0,0 +1,70 @@
+import { EventDispatcher } from "../domain/event/@shared/eventDispatcher";
+import { SendEmailWhenProductIsCreatedHandler } from "../domain/event/product/handler/sendEmailWhenProductIsCreatedHandler";
+import { ProductCreatedEvent } from "../domain/event/product/productCreatedEvent";
+
+describe("Domain Events tests", () => {
+    it("Should register an event handler", () => {
+
+        const eventDispatcher = new EventDispatcher();
+        const eventHandler = new SendEmailWhenProductIsCreatedHandler();
+        eventDispatcher.register("ProductCreatedEvent", eventHandler);
+        expect(eventDispatcher.getEventHandlers["ProductCreatedEvent"]).toBeDefined();
+        expect(eventDispatcher.getEventHandlers["ProductCreatedEvent"]).toHaveLength(1);
+        expect(eventDispatcher.getEventHandlers["ProductCreatedEvent"][0]).toMatchObject(eventHandler);
+
+
+    })
+
+    it("Should unregister an event handler", () => {
+        const eventDispatcher = new EventDispatcher();
+        const eventHandler = new SendEmailWhenProductIsCreatedHandler();
+
+        eventDispatcher.register("ProductCreatedEvent", eventHandler);
+        expect(eventDispatcher.getEventHandlers["ProductCreatedEvent"]).toBeDefined();
+        expect(eventDispatcher.getEventHandlers["ProductCreatedEvent"]).toHaveLength(1);
+        expect(eventDispatcher.getEventHandlers["ProductCreatedEvent"][0]).toMatchObject(eventHandler);
+
+        eventDispatcher.unregister("ProductCreatedEvent", eventHandler)
+        expect(eventDispatcher.getEventHandlers["ProductCreatedEvent"]).toHaveLength(0);
+        expect(eventDispatcher.getEventHandlers["ProductCreatedEvent"]).toBeUndefined;
+
+    });
+
+    it("Should unregister all events", () => {
+        const eventDispatcher = new EventDispatcher();
+        const eventHandler = new SendEmailWhenProductIsCreatedHandler();
+
+        eventDispatcher.register("ProductCreatedEvent", eventHandler);
+        expect(eventDispatcher.getEventHandlers["ProductCreatedEvent"]).toBeDefined();
+        expect(eventDispatcher.getEventHandlers["ProductCreatedEvent"]).toHaveLength(1);
+        expect(eventDispatcher.getEventHandlers["ProductCreatedEvent"][0]).toMatchObject(eventHandler);
+
+        eventDispatcher.unregisterAll();
+
+        expect(eventDispatcher.getEventHandlers["ProductCreatedEvent"]).toBeUndefined;
+
+    });
+
+    it("Should notify all event handlers", () => {
+        const eventDispatcher = new EventDispatcher();
+        const eventHandler = new SendEmailWhenProductIsCreatedHandler();
+        const spyEventHandler = jest.spyOn(eventHandler, "handle");
+        eventDispatcher.register("ProductCreatedEvent", eventHandler);
+
+        expect(eventDispatcher.getEventHandlers["ProductCreatedEvent"][0]).toMatchObject(eventHandler);
+
+        const productCreatedEvent = new ProductCreatedEvent({
+            name: "Product one",
+            description: "Product description",
+            price: 10.00,
+            email: "charles.nelson@gmail.com"
+        });
+
+        eventDispatcher.notify(productCreatedEvent);
+
+        expect(spyEventHandler).toHaveBeenCalled();
+
+
+
+    })
+})
