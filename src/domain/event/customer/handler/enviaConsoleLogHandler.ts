35d8d809e9e4d8ad7567df01fddacd0cc100eb77
Wander Aquino <wander.aquino@gmail.com>
2023-06-26 17:45:00 -0300

    Include addressChangedEvent as type to EventHandlerInterface

--- src/domain/event/customer/handler/enviaConsoleLogHandler.ts
+++ src/domain/event/customer/handler/enviaConsoleLogHandler.ts
@@ -1,7 +1,8 @@
 import { EventHandlerInterface } from "../../@shared/eventHandlerInterface";
 import { EventInterface } from "../../@shared/eventInterface";
+import { AddressChangedEvent } from "../addressChangedEvent";
 
-export class EnviaAddressConsoleLogHandler implements EventHandlerInterface {
+export class EnviaAddressConsoleLogHandler implements EventHandlerInterface <AddressChangedEvent> {
     handle(event: EventInterface): void {
         console.log(`Endereço do cliente ${event.eventData.customerId}, ${event.eventData.customerName} alterado para: 
             ${event.eventData.address.street}, ${event.eventData.address.number}, ${event.eventData.address.city}`);
