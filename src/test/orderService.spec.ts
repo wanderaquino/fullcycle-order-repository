b8ef2661bc4b570dd0ca01159f35811e785355f6
Wander Aquino <wander.aquino@gmail.com>
2023-06-25 23:45:16 -0300

    Move file to test folder

---
+++ src/test/orderService.spec.ts
@@ -0,0 +1,36 @@
+import { Customer } from "../domain/entity/customer";
+import { Order } from "../domain/entity/order";
+import { OrderItem } from "../domain/entity/orderItem"
+import { OrderService } from "../domain/service/orderService";
+import {v4 as uuid} from "uuid";
+
+
+describe("OrderService unit tests", () => {
+    it("Should throw new error when items size is less than one", () => {
+        expect(() => {
+            OrderService.placeOrder(new Customer(uuid(), "Customer Charles"), [])
+        }).toThrowError("At least one item must be informed");
+    });
+
+    it("Should get total of all orders", () => {
+        const orderItem01 = new OrderItem("Item01", "Item 01", 100, "p1", 1);   
+        const orderItems02 = new OrderItem("Item02", "Item 02", 200, "p2", 1);
+
+        const order = new Order("order01", "123", [orderItem01]);
+        const order02 = new Order("order02", "455", [orderItems02]);
+
+        const total = OrderService.total([order, order02]);
+
+        expect(total).toBe(300);
+    });
+
+    it("Should apply reward points to customer", () => {
+        const orderItem01 = new OrderItem("Item01", "Item 01", 100, "p1", 1);   
+        const orderItems02 = new OrderItem("Item02", "Item 02", 200, "p2", 1);
+        const customer = new Customer(uuid(), "Customer Charles");
+
+        const order = OrderService.placeOrder(customer, [orderItem01, orderItems02]);
+
+        expect(customer.getRewardPoints()).toBe(150);
+    })
+})
---
+++ src/test/product.service.spec.ts
@@ -0,0 +1,16 @@
+import { Product } from "../domain/entity/product";
+import { ProductService } from "../domain/service/productService";
+
+describe("ProducService Unit Tests", () => {
+    it("Should change the prices of all products", () => {
+        const products = [];
+        const productOne = new Product("ProductOne", "Product 1", 10);
+        const productTwo = new Product("ProductTwo", "Product 2", 50);
+        products.push(productOne, productTwo);
+
+        ProductService.IncreasePrice(products, 100);
+
+        expect(productOne.price).toBe(20);
+        expect(productTwo.price).toBe(100);
+    })
+})
