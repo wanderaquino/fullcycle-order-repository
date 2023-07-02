9580b7e339c2a0b59af97888fd6ce17535b9c3f2
Wander Aquino <wander.aquino@gmail.com>
2023-07-02 14:49:43 -0300

    Fix import

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,10 @@
-import {Customer} from "./domain//entity/customer";
-import {Address} from "./domain//entity/address";
-import { OrderItem } from "./domain/entity/orderItem";
-import { Order } from "./domain/entity/order";
+import {Customer} from "./domain/customer/entity/customer";
+import {Address} from "./domain/customer/value-object/address";
+import { OrderItem } from "./domain/checkout/entity/orderItem";
+import { Order } from "./domain/checkout/entity/order";
 
 let customer = new Customer("123", "Wander Aquino");
-const address = new Address("Rua Dr Backer Alfred", 458, "24452005", "Rio de Janeiro", "Brasil", true);
+const address = new Address("Rua Dr Backer Alfred", 458, "24452005", "Rio de Janeiro", "Brasil");
 const orderItem = new OrderItem("123", "Item 01", 1699, "123", 5);
 const orderItem2 = new OrderItem("123", "Item 02", 1099, "123", 5);
 const order = new Order("123","999",[orderItem, orderItem2]);
